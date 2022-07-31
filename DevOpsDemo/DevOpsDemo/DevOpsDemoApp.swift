8cdafd4efeb32a9c5d84bab3192a4099bd54a8db
Jitender Kumar <jitender.k123@hotmail.com>
2022-07-31 11:52:42 +0800

    first commit

---
+++ DevOpsDemo/DevOpsDemo/DevOpsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevOpsDemoApp.swift
+//  DevOpsDemo
+//
+//  Created by Jitender Kumar on 31/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DevOpsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
