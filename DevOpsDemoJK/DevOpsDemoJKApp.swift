4194f93c1c3252c57c418805e3f8d6a4e673fddd
Jitender Kumar <jitender.k123@hotmail.com>
2022-08-01 05:17:18 +0800

    Test case fixed

---
+++ DevOpsDemoJK/DevOpsDemoJKApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevOpsDemoJKApp.swift
+//  DevOpsDemoJK
+//
+//  Created by Jitender Kumar on 31/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DevOpsDemoJKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
